68b574930e1ea2b092551db984c55b5d141e93d0
Danilo Kulišić <danilokulisic4@gmail.com>
2025-09-20 01:09:49 +0200

    adding contact form. useState, handleSubmit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,4 @@
+import ContactForm from '@/components/ContactForm'
 import React from 'react'
 
 const contact = () => {
@@ -5,6 +6,7 @@ const contact = () => {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col row-start-2 items-center sm:items-start"> 
         <h1 className="text-4xl font-bold text-blue-600">Get In Touch</h1>
+          <ContactForm />
       </main>
     </div>
   )
