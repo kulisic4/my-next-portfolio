a49e62c63e2ac69d09c93ef6953f0fc0c021badb
Danilo Kulišić <danilokulisic4@gmail.com>
2025-09-25 22:54:40 +0200

    fixing h2 message in contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,7 +5,6 @@ const contact = () => {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col row-start-2 items-center sm:items-start"> 
-        <h1 className="text-4xl font-bold text-blue-600">Get In Touch</h1>
           <ContactForm />
       </main>
     </div>
