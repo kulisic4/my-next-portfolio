4f8d18abd459f884ffcb2ea496b12711b55cf608
Danilo Kulišić <danilokulisic4@gmail.com>
2025-09-26 01:09:38 +0200

    adding weather API component

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,26 +1,22 @@
 'use client'
 import Link from 'next/link'
 import React from 'react'
+import WeatherWidget from './WeatherWidget'
 
 export default function Navbar() {
   return (
-    <nav className="bg-black text-white font-bold p-4 flex justify-between items-center">
+    <nav className="bg-black text-white font-bold p-4 flex justify-between items-center relative">
       <div className="text-xl font-bold">
         <Link href="/">Danilo</Link>
       </div>
+      <div className="absolute left-1/2 transform -translate-x-1/2">
+        <WeatherWidget />
+      </div >
       <ul className="flex space-x-6">
-        <li>
           <Link href="/" className="hover:text-sky-200">Home</Link>
-        </li>
-        <li>
           <Link href="/about" className="hover:text-sky-200">About</Link>
-        </li>
-        <li>
           <Link href="/projects" className="hover:text-sky-200">Apps</Link>
-        </li>
-        <li>
           <Link href="/contact" className="hover:text-sky-200">Contact</Link>
-        </li>
       </ul>
     </nav>
   )
