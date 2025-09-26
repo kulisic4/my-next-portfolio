314e626612d829222ddcfefdd5fdaf19ff3a796f
Danilo Kulišić <danilokulisic4@gmail.com>
2025-09-27 00:25:49 +0200

    creating ProjectCarousel component and adding png's

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/NavBar";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
