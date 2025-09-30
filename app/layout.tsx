439019779774abc91d088b74d7226f06a4109587
Danilo Kulišić <danilokulisic4@gmail.com>
2025-09-30 13:42:59 +0200

    adding Footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,30 @@
+'use client'
+import React from 'react'
+import { Github, Linkedin, Mail } from 'lucide-react'
+
+export default function Footer() {
+  return (
+    <footer className="bg-black text-white py-8 mt-12">
+      <div className="max-w-6xl mx-auto px-6 flex flex-col sm:flex-row justify-between items-center gap-6">
+        
+        {/* Logo / ime */}
+        <div className="text-lg font-bold">
+          © {new Date().getFullYear()} Danilo Kulišić
+        </div>
+
+        {/* Ikonice */}
+        <div className="flex gap-4">
+          <a href="https://github.com/kulisic4" target="_blank" rel="noopener noreferrer" className="hover:text-sky-200">
+            <Github />
+          </a>
+          <a href="https://www.linkedin.com/in/danilo-kulisic4/" target="_blank" rel="noopener noreferrer" className="hover:text-sky-200">
+            <Linkedin />
+          </a>
+          <a href="mailto:danilokulisic4@gmail.com" className="hover:text-sky-200">
+            <Mail />
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
