b75e6d7c4c9ab2a0ee947c1d9c01b7592a3cd8da
Danilo Kulišić <danilokulisic4@gmail.com>
2026-06-16 13:22:37 +0200

    hiding weather widget component import

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link'
 import React, { useState } from 'react'
-import WeatherWidget from './WeatherWidget'
+//import WeatherWidget from './WeatherWidget'
 import { Menu, X } from 'lucide-react'
 import { motion, AnimatePresence } from 'framer-motion'
 
